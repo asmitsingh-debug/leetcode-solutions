82daf7104be8ca8a0ba0e388c58f4454b2ed38ee
asmitsingh-debug <smhs10asmitsingh10@gmail.com>
2026-08-31 10:16:08 +0530

    Time: 393 ms (65.08%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH t AS (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+)
+SELECT id, visit_date, people
+FROM t
+WHERE grp IN (
+    SELECT grp
+    FROM t
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+ORDER BY visit_date;
